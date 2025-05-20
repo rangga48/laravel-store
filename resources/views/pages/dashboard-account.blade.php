0643a6dd251416adb511545c8ffed3f0c968c369
rangga48 <ranggaputra681@gmail.com>
2025-05-20 22:32:06 +0700

    .

--- resources/views/pages/dashboard-account.blade.php
+++ resources/views/pages/dashboard-account.blade.php
@@ -3,14 +3,14 @@
 @section('title')
     Dashboard Account Page
 @endsection
-          
+
 @section('content')
           <div class="section-content section-dashboard-home" data-aos="fade-up">
             <div class="container-fluid">
               <div class="dashboard-heading">
                 <h2 class="dashboard-title">My Account</h2>
                 <p class="dashboard-subtitle">
-                  Update your current profile
+                  Update your current profile!
                 </p>
               </div>
               <div class="dashboard-content">
